4f2a68b9b67f797749400e55d7b73f205a1590f7
notaspect372 <notaspect372@gmail.com>
2024-11-17 16:18:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ headers = {
 geolocator = Nominatim(user_agent="property_scraper")
 
 start_urls = [
-    "https://www.bproperty.com/buy/residential/residential-duplex/residential-plazas/residential-buildings/residential-plots/"
+    "https://www.bproperty.com/rent/commercial/under-70000/"
 ]
 
 # Function to perform requests with manual retry logic
